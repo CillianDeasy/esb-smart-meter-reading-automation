b4add6edab97122c72330b2d68ef89d86b5b984f
Colin MacGiollaEain <colin@arista.com>
2024-01-02 20:28:51 +0000

    Initial conversion

--- esb-smart-meter-reader.py
+++ esb-smart-meter-reader.py
@@ -7,105 +7,138 @@
 # Modified by badger707
 # it works as of 21-JUL-2023
 
+import sys
 import requests
 from bs4 import BeautifulSoup
 import re
 import json
 import csv
-from datetime import datetime, timedelta, timezone
+import configparser
+from datetime import datetime
+import logging
 
-def load_esb_data(user, password, mpnr, start_date):
-  print("[+] open session ...")
-  s = requests.Session()
-  s.headers.update({
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
-  })    
-  print("[+] calling login page. ..")
-  login_page = s.get('https://myaccount.esbnetworks.ie/', allow_redirects=True)
-  result = re.findall(r"(?<=var SETTINGS = )\S*;", str(login_page.content))
-  settings = json.loads(result[0][:-1])
-  print("[+] sending credentials ...")
-  s.post(
-    'https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/SelfAsserted?tx=' + settings['transId'] + '&p=B2C_1A_signup_signin', 
-    data={
-      'signInName': user, 
-      'password': password, 
-      'request_type': 'RESPONSE'
-    },
-    headers={
-      'x-csrf-token': settings['csrf'],
-    },
-    allow_redirects=False)
-  print("[+] passing AUTH ...")
-  confirm_login = s.get(
-    'https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/api/CombinedSigninAndSignup/confirmed',
-    params={
-      'rememberMe': False,
-      'csrf_token': settings['csrf'],
-      'tx': settings['transId'],
-      'p': 'B2C_1A_signup_signin',
-    }
-  )
-  print("[+] confirm_login: ",confirm_login)
-  print("[+] doing some BeautifulSoup ...")
-  soup = BeautifulSoup(confirm_login.content, 'html.parser')
-  form = soup.find('form', {'id': 'auto'})
-  s.post(
-    form['action'],
-    allow_redirects=False,
-    data={
-      'state': form.find('input', {'name': 'state'})['value'],
-      'client_info': form.find('input', {'name': 'client_info'})['value'],
-      'code': form.find('input', {'name': 'code'})['value'],
-    }, 
-  )
+
+class EsbDataCollection:
   
-  #data = s.get('https://myaccount.esbnetworks.ie/datadub/GetHdfContent?mprn=' + mpnr + '&startDate=' + start_date.strftime('%Y-%m-%d'))
-  print("[+] getting CSV file for MPRN ...")
-  data = s.get('https://myaccount.esbnetworks.ie/DataHub/DownloadHdf?mprn=' + mpnr + '&startDate=' + start_date.strftime('%Y-%m-%d'))
+  def __init__(self, username, password, mprn) -> None:
+    self.username = username
+    self.password = password
+    self.mprn = mprn
+    self.log = logging.getLogger('EsbDataCollection')
+    
+    _h = logging.StreamHandler(sys.stdout)
+    _h.setLevel(logging.INFO)
+    _h.setFormatter(logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    
+    self.log.addHandler(_h)
+    
+    self.csv = None
+    self.json = None
+    
 
-  print("[+] CSV file received !!!")
-  data_decoded = data.content.decode('utf-8').splitlines()
-  print("[+] data decoded from Binary format")
-  json_data = csv_response_to_json(data_decoded)
-  return json_data
+  def __load_esb_data(self, start_date):
+    self.log.info('Opening session...')
+    s = requests.Session()
+    s.headers.update({
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+    }) 
+    
+    self.log.info('Getting login page')
+    try:
+      login_page = s.get('https://myaccount.esbnetworks.ie/', allow_redirects=True)
+      result = re.findall(r"(?<=var SETTINGS = )\S*;", str(login_page.content))
+      settings = json.loads(result[0][:-1])
+    except Exception as e:
+      self.log.error(e)
+    
+    self.log.info('Sending credentials')
+    try:
+      s.post(
+        'https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/SelfAsserted?tx=' + settings['transId'] + '&p=B2C_1A_signup_signin', 
+        data={
+          'signInName': self.username, 
+          'password': self.password, 
+          'request_type': 'RESPONSE'
+        },
+        headers={
+          'x-csrf-token': settings['csrf'],
+        },
+        allow_redirects=False)
+    except Exception as e:
+      self.log.error(e)
+    self.log.info('Passing authentication')
+    try:
+      confirm_login = s.get(
+        'https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/api/CombinedSigninAndSignup/confirmed',
+        params={
+          'rememberMe': False,
+          'csrf_token': settings['csrf'],
+          'tx': settings['transId'],
+          'p': 'B2C_1A_signup_signin',
+        }
+      )
+    except Exception as e:
+      self.log.error(e)
+    
+    self.log.debug('login confirmed: %s' % confirm_login)
+    self.log.info('parsing content')
+    soup = BeautifulSoup(confirm_login.content, 'html.parser')
+    form = soup.find('form', {'id': 'auto'})
+    s.post(
+      form['action'],
+      allow_redirects=False,
+      data={
+        'state': form.find('input', {'name': 'state'})['value'],
+        'client_info': form.find('input', {'name': 'client_info'})['value'],
+        'code': form.find('input', {'name': 'code'})['value'],
+      }, 
+    )
+    
+    self.log.info('Getting data using v2 URL')
+    try:
+      data = s.get('https://myaccount.esbnetworks.ie/DataHub/DownloadHdf?mprn=' + self.mprn + '&startDate=' + start_date.strftime('%Y-%m-%d'))
+    except Exception as e:
+      self.log.error('Failed to retrieve data using v2 endpoint: %s' % e)
+      
+    data_decoded = data.content.decode('utf-8').splitlines()
+    
+    return data_decoded
 
-def csv_response_to_json(csv_file):
-  print("[+] creating JSON file from CSV ...")
-  my_json = []
-  csv_reader = csv.DictReader(csv_file)
-  for row in csv_reader:
-    my_json.append(row)
-  with open("json_data.json","w",encoding="utf-8") as jsonf:
-     json_out = json.dumps(my_json, indent=2)
-     jsonf.write(json_out)
-  print("[+] end of JSON OUT.",json_out)
-  print("[+] end of JSON OUT, returning value ...")
-  return json_out
 
-def parse_date(date_str):
-  print("[+] parsing some data fields ...")
-  if len(date_str) == 19:
-      return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
-  else:
-      dt = datetime.strptime(date_str[:19], '%Y-%m-%dT%H:%M:%S')
-      tz_offset = int(date_str[-6:-3])
-      tz = timezone(timedelta(hours=tz_offset))
-      return dt.replace(tzinfo=tz)
+  def get_csv_data(self):
+    today = datetime.today()
+    
+    if self.csv is None:
+      self.csv = self.__load_esb_data(today)
+    
+    return self.csv
+  
+  def get_json_data(self):
+    
+    if self.json is None:
+      self.json = []
+    
+      csv_reader = csv.DictReader( self.get_csv_data() )
+      for row in csv_reader:
+        self.json.append(row)
+      
+    return self.json
+    
 
-def load_smart_meter_stats_v2(user, password, mpnr):
-  #last_month = datetime.today() - timedelta(days=30)
-  today_ = datetime.today()
-  #smart_meter_data = load_esb_data(user, password, mpnr, last_month)
-  smart_meter_data = load_esb_data(user, password, mpnr, today_)
-  print("[+] smart_meter_data: ",smart_meter_data)
-  print("[++] end of smart_meter_data")
-  return smart_meter_data
+def main():
+  config = configparser.ConfigParser()
+  config.read('.secrets')
+  
+  esb = EsbDataCollection(config['esb']['USER'], config['esb']['PASSWORD'], config['esb']['MPRN'] )
+  
+  records = esb.get_csv_data()
+  
+  
+  sys.exit()
+  
+  
 
-meter_mprn = "your-meter-mprn-number"
-esb_user_name = "your-esb-account@email.com"
-esb_password = "your-esb-account-password"
 
-xoxo = load_smart_meter_stats_v2(esb_user_name, esb_password, meter_mprn)
-print("[XOXO]: ",xoxo)
-print("[END] XOXO")
+
+if __name__ == "__main__":
+    main()
